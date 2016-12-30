0e6b583e2dd8d621a02e8b9ee571e31d0c659d0a
Adrian Rodriguez <contact@adrianrodriguez.us>
2016-12-30 09:08:25 -0800

    Pep 8 Compliant

--- index.py
+++ index.py
@@ -2,43 +2,63 @@ import media
 import fresh_tomatoes
 
 # Inception
-incenption = media.Movie("Inception",
-	"A professional thief who steals information by infiltrating people's dreams",
-	"https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-	"https://www.youtube.com/watch?v=8hP9D6kZseM")
+incenption = media.Movie(
+    "Inception",
+    "A professional thief who steals information by infiltrating"
+    " people's dreams",
+    "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29"
+    "_theatrical_poster.jpg",
+    "https://www.youtube.com/watch?v=8hP9D6kZseM")
 
 # Siete Viergenes
-siete_virgenes = media.Movie("7 Virgenes",
-	"A teenager is given a 48 hour leave from a juvenile reform center to attend his brother weeding",
-	"http://pics.filmaffinity.com/7_virgenes-483661126-large.jpg",
-	"https://www.youtube.com/watch?v=29txqk7I-0U")
+siete_virgenes = media.Movie(
+    "7 Virgenes",
+    "A teenager is given a 48 hour leave from a juvenile reform "
+    "center to attend his brother weeding",
+    "http://pics.filmaffinity.com/7_virgenes-483661126-large.jpg",
+    "https://www.youtube.com/watch?v=29txqk7I-0U")
 
 # Interstellar
-interstellar = media.Movie("Interstellar", 
-	"Astronauts travel through a wormhole in search of a new home for humanity", 
-	"http://t1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB", 
-	"https://www.youtube.com/watch?v=SIZpLFPQLLg")
+interstellar = media.Movie(
+    "Interstellar",
+    "Astronauts travel through a wormhole in search of a "
+    "new home for humanity",
+    "http://t1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-Vo"
+    "gMH8LfZHEaq3UdCMLxARZAB",
+    "https://www.youtube.com/watch?v=SIZpLFPQLLg")
 
 # Y Tu Mama Tambien
-y_tu_mama_tambien = media.Movie("Y Tu Mama Tambien",
-	"A story about two teenage boys who take a road trip with a woman in her late twenties", 
-	"https://upload.wikimedia.org/wikipedia/en/6/63/Y_tu_mam%C3%A1_tambi%C3%A9n_poster.png",
-	"https://www.youtube.com/watch?v=pdh8Va2oDKk")
+y_tu_mama_tambien = media.Movie(
+    "Y Tu Mama Tambien",
+    "A story about two teenage boys who take a road trip with a woman "
+    "in her late twenties",
+    "https://upload.wikimedia.org/wikipedia/en/6/63/Y_tu_mam%C3%A1_tam"
+    "bi%C3%A9n_poster.png",
+    "https://www.youtube.com/watch?v=pdh8Va2oDKk")
 
 # Ex Machina
-ex_machina = media.Movie("Ex Machina",
-	"A progammer test a humanoid robot if is capable of though and consciousness",
-	"https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
-	"https://www.youtube.com/watch?v=bggUmgeMCdc")
+ex_machina = media.Movie(
+    "Ex Machina",
+    "A progammer test a humanoid robot if is capable of though "
+    "and consciousness",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTI"
+    "xMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    "https://www.youtube.com/watch?v=bggUmgeMCdc")
 
 # Victoria
-victoria = media.Movie("Victoria",
-	"A young Spanish woman who has newly moved to Berlin finds her flirtation with a local guy turn potentially deadly as their night out with his friends reveals a dangerous secret",
-	"https://upload.wikimedia.org/wikipedia/en/f/fe/Victoria_%282015_film%29_POSTER.jpg",
-	"https://www.youtube.com/watch?v=Kp8wcV3GjW0")
+victoria = media.Movie(
+    "Victoria",
+    "A young Spanish woman who has newly moved to Berlin finds her "
+    "flirtation with a local guy turn potentially deadly as their "
+    "night out with his friends reveals a dangerous secret",
+    "https://upload.wikimedia.org/wikipedia/en/f/fe/Victoria_%282"
+    "015_film%29_POSTER.jpg",
+    "https://www.youtube.com/watch?v=Kp8wcV3GjW0")
 
 # Create a list of movies
-movies = [incenption, siete_virgenes, interstellar, y_tu_mama_tambien, ex_machina, victoria]
+movies = [incenption, siete_virgenes, interstellar,
+          y_tu_mama_tambien, ex_machina, victoria]
 
-# Generate web page 
+# Generate web page
+
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,17 +1,21 @@
 import webbrowser
 
 # Define the movie class
+
+
 class Movie():
-  """ This class provides a way to store movie related information"""
 
-  def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-    
-    # Create instance variables and assign value
-    self.title = movie_title
-    self.storyline = movie_storyline
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
+    """ This class provides a way to store movie related information """
 
-  # Define an instance method
-  def show_trailer(self):
-    webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+
+        # Create instance variables and assign value
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    # Define an instance method
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
